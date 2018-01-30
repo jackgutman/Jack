e4cefe1b7fec7072033704d9db238a0c150f2519
Jack <jack_gutman@dalton.org>
2018-01-30 11:42:29 -0500

    Do it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Jack Gutman is a lord");
+	System.out.println("Jack Gutman is a lord"
+    );
     }
 }
